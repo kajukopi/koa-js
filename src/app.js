6f85fb250b2de9ae9e1575c1615f9789989a4f8c
Kaju Kopi <kajukopi@gmail.com>
2024-08-14 20:43:00 +0700

    Update

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
-import {config} from "dotenv"
+import { config } from "dotenv"
 import Koa from "koa"
 import bodyParser from "koa-bodyparser"
 import mongoose from "mongoose"
-import renderer from "koa-hbs-renderer"
-import Handlebars from 'handlebars';
-import path from "path"
-import {fileURLToPath} from "url"
 import serve from "koa-static"
-import {logger} from "./middlewares/handler.js"
+import path from "path"
+import { fileURLToPath } from "url"
+import render from "koa-hbs-renderer";
+import Handlebars from "handlebars";
+
+import { logger } from "./middlewares/handler.js"
 import routerItem from "./routes/items.js"
 import routerHome from "./routes/home.js"
 import routerAsset from "./routes/assets.js"
 import routerAuth from "./routes/auth.js"
+
 
 config()
 
@@ -25,42 +27,33 @@ mongoose.connect(process.env.DATABASE_URL)
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-// Serve static files from the "public" directory
-app.use(serve(path.join(__dirname, "..", 'assets'),{
-  maxage: 1000 * 60 * 60 * 24, // Cache for 1 day
-  hidden: false, // Do not serve hidden files
-  gzip: true, // Serve gzip versions of files if available
+app.use(serve(path.join(__dirname, "..", 'assets'), {
+  maxage: 1000 * 60 * 60 * 24,
+  hidden: false,
+  gzip: true,
 }));
-
-
-let options = {
-  cacheExpires:  60,
-  contentTag:    'content',
+const options = {
+  cacheExpires: 60,
+  contentTag: 'content',
   defaultLayout: 'default',
-  environment:   'development',
-  extension:     '.hbs',
-  hbs:           Handlebars.create(),
+  environment: 'development',
+  extension: '.hbs',
+  hbs: Handlebars.create(),
   paths: {
-    views:    path.join(__dirname, 'views'),
-    layouts:  path.join(__dirname, 'views/layouts'),
-    partials: path.join(__dirname, 'views/partials'),
-    helpers:  path.join(__dirname, 'views/helpers')
+    views: path.join(__dirname, 'views'),
+    layouts: path.join(__dirname, "views", 'layouts'),
+    partials: path.join(__dirname, "views", 'partials'),
   },
   Promise: Promise
-};
+}
 
-
-// Setup the template engine
-app.use(
-  renderer(options)
-)
-
+// Set up views with koa-hbs-render
+app.use(render(options));
 
 app.use(bodyParser())
 app.use(logger(":method :url"))
 
 // Use router middleware
-app.use(routerItem.middleware())
 app.use(routerHome.routes()).use(routerHome.allowedMethods())
 app.use(routerItem.routes()).use(routerItem.allowedMethods())
 
--- src/routes/home.js
+++ src/routes/home.js
@@ -3,9 +3,9 @@ import Item from "../models/Item.js"
 const router = new Router()
 
 // Define routes
-router.get("/", async (ctx, next) => {
+router.get("/", async (ctx) => {
   const items = await Item.find().lean()
-  await ctx.render("index", {items})
+  await ctx.render("home", { items })
 })
 
 export default router
